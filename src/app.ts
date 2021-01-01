2d426883274649e2cacbcdf09993652fda9b6b6e
Qayyuum Harun <qayyuum.harun@gmail.com>
2021-01-01 22:56:55 +0800

    Add handler to inputs

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,33 @@ class ProjectInput {
   templateHTML: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateHTML = document.getElementById('project-input')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
-    
-    const importedNode =  document.importNode(this.templateHTML.content, true);
+
+    const importedNode = document.importNode(this.templateHTML.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
