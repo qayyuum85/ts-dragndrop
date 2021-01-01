79832a172eb67a24b03dc2d0a9f071193fb8471b
Qayyuum Harun <qayyuum.harun@gmail.com>
2021-01-01 22:37:53 +0800

    Attach template to app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class ProjectInput {
+  templateHTML: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateHTML = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    
+    const importedNode =  document.importNode(this.templateHTML.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const newProject = new ProjectInput();
