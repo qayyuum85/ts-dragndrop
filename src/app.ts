3a672d326d899325c938f533ec64ce8a78e98ab9
Qayyuum Harun <qayyuum.harun@gmail.com>
2021-01-16 16:33:14 +0800

    Add getter to project item

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ type Listener<T> = (items: T[]) => void;
 
 class State<T> {
   protected listeners: Listener<T>[] = [];
-  
+
   addListener(listenerFn: Listener<T>) {
     this.listeners.push(listenerFn);
   }
@@ -124,19 +124,27 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
-  constructor(hostId:string, project:Project){
-    super('single-project',hostId, false, project.id )
+
+  get persons() {
+    if (this.project.people === 1) {
+      return `1 person`;
+    }
+
+    return `${this.project.people} persons`;
+  }
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
     this.project = project;
 
     this.configure();
     this.renderContent();
   }
 
-  configure(){}
+  configure() {}
 
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
@@ -153,7 +161,8 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   }
 
   renderContent() {
-    const listId = `${this.type}-projects-list`;``
+    const listId = `${this.type}-projects-list`;
+    ``;
     this.element.querySelector('ul')!.id = listId;
     this.element.querySelector('h2')!.textContent = `${this.type.toUpperCase()} PROJECTS`;
   }
@@ -175,7 +184,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-projects-list`) as HTMLUListElement;
     listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
-      new ProjectItem(this.element.querySelector("ul")!.id, prjItem)
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
 }
