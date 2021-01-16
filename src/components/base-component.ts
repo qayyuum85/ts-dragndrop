5da9de94da11d138d0f5f162dd72db3644a8a927
Qayyuum Harun <qayyuum.harun@gmail.com>
2021-01-16 23:07:33 +0800

    Organise using namespace

---
+++ src/components/base-component.ts
@@ -0,0 +1,27 @@
+namespace App {
+  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateHTML: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(templateId: string, hostId: string, insertAtStart: boolean, newElementId?: string) {
+      this.templateHTML = document.getElementById(templateId)! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostId)! as T;
+
+      const importedNode = document.importNode(this.templateHTML.content, true);
+      this.element = importedNode.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
---
+++ src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+  export function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const mutatedMethod: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+
+    return mutatedMethod;
+  }
+}
